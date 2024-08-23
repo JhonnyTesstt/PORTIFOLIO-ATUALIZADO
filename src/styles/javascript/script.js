fa6299ac8a2e9db1af4d7eb85db659cf06ae4077
JhonnyTesstt <sousa-jv@hotmail.com>
2024-08-23 00:55:44 -0300

    projeto atualizado

--- src/styles/javascript/script.js
+++ src/styles/javascript/script.js
@@ -1,6 +1,18 @@
+//Botão do Menu-Mobile
 $(document).ready(function(){
     $('#mobile_btn').on('click', function(){
         $('#mobile_menu').toggleClass('active');
         $('#mobile_btn').find('i').toggleClass('fa-x');
     })
 });
+
+
+//Efeito ScrollReveal
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.efeito-txt',{
+    duration: 3000,
+    distance: '100px'
+})
+
+ScrollReveal().reveal('.efeito-txt');
